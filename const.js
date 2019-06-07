cabb64a408b1985abfee22014ac1814087f1710f
feesech <mikesokolov@me.com>
2019-06-07 13:49:08 +0300

    Add files via upload

---
+++ const.js
@@ -0,0 +1,19 @@
+// variables globales
+var blocktype;
+var ismunicipal;
+var population;
+var business;   // = json amount
+var distance;   // = json width
+var activity = 0;
+
+var bldsz = ["small", "medium", "large"];
+var buildingsize = [72, 144, 216];
+var bldKoef = [1, 1.5, 2];
+
+//стоимость здания от размера
+var buildCost = [250000];
+// brew kettle цена за 1,2,3
+var brewKettle = [80000, 155000, 230000];
+
+var furniture = [75000, 100000, 130000];
+var demand;
